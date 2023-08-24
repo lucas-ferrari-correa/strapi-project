cba3060b42b8be258ead6dd1159734b115ecc8e7
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2023-08-24 14:58:19 -0300

    extending strapi core 3

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -4,4 +4,25 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::post.post');
+export default factories.createCoreController('api::post.post', ({ strapi }) => ({
+  async find(ctx) {
+    const isRequestingNonPremium = ctx.query.filters && ctx.query.filters.premium["$eq"] == "false";
+
+    if (ctx.state.user || isRequestingNonPremium) {
+      return await super.find(ctx);
+    }
+
+    const { query } = ctx;
+
+    const filteredPosts = await strapi.service("api::post.post").find({
+      ...query,
+      filters: {
+        ...query.filters,
+        premium: false
+      }
+    });
+
+    const sanitizedPosts = await this.sanitizeOutput(filteredPosts, ctx);
+    return this.transformResponse(sanitizedPosts);
+  }
+}));
