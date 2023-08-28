ae91e242bb2866cfdc65719a9b78beba77f36bb3
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2023-08-28 17:04:41 -0300

    strapi core part 2

--- src/api/post/controllers/post.ts
+++ src/api/post/controllers/post.ts
@@ -12,17 +12,50 @@ export default factories.createCoreController('api::post.post', ({ strapi }) => 
       return await super.find(ctx);
     }
 
+    const publicPosts = await strapi
+      .service('api::post.post')
+      .findPublic(ctx.query);
+
+    const sanitizedPosts = await this.sanitizeOutput(publicPosts, ctx);
+    return this.transformResponse(sanitizedPosts);
+  },
+
+  async findOne(ctx) {
+    if (ctx.state.user) {
+      return await super.findOne(ctx);  
+    }
+    
+    const { id } = ctx.params;
     const { query } = ctx;
 
-    const filteredPosts = await strapi.service("api::post.post").find({
-      ...query,
-      filters: {
-        ...query.filters,
-        premium: false
-      }
-    });
+    const postIfPublic = await strapi
+      .service('api::post.post')
+      .findOneIfPublic({
+        id, query
+      })
 
-    const sanitizedPosts = await this.sanitizeOutput(filteredPosts, ctx);
-    return this.transformResponse(sanitizedPosts);
+    const sanitizeEnitity = this.sanitizeOutput(postIfPublic, ctx);
+    return this.transformResponse(sanitizeEnitity)
+  },
+
+  async likePost(ctx) {
+    if (!ctx.state.user) {
+      return ctx.forbidden('Only authenticated users can post')
+    }
+
+    const user = ctx.state.user;
+    const postId = ctx.params.id;
+    const { query } = ctx;
+
+    const updatedPost = await strapi
+      .service('api::post.post')
+      .likePost({
+        postId,
+        userId: user.id,
+        query,
+      })
+
+    const sanitizeEnitity = this.sanitizeOutput(updatedPost, ctx);
+    return this.transformResponse(sanitizeEnitity)
   }
 }));
--- src/api/post/services/post.ts
+++ src/api/post/services/post.ts
@@ -4,4 +4,52 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::post.post');
+export default factories.createCoreService('api::post.post', ({ strapi }) => ({
+  async findPublic(args: any) {
+    const newQuery = {
+      ...args,
+      filters: {
+        ...args.filters,
+        premium: false
+      }
+    };
+
+    const publicPosts = await strapi
+      .entityService
+      .findMany('api::post.post', this.getFetchParams(newQuery))
+  },
+
+  async findOneIfPublic(args: any) {
+    const { id, query } = args;
+
+    const post = await strapi
+      .entityService
+      .findOne('api::post.post', id, this.getFetchParams(query))
+
+    return post.premium ? null : post
+  },
+
+  async likePost(args) {
+    const { postId, userId, query } = args;
+
+    const postToLike = await strapi
+      .entityService
+      .findOne('api::post.post', postId, {
+        populate: ['likedBy']
+      })
+
+    const updatedPost = await strapi
+      .entityService
+      .update('api::post.post', postId, {
+        data: {
+          likedBy: [
+            ...postToLike.likedBy,
+            userId
+          ]
+        },
+        ...query
+      })
+
+    return updatedPost
+  }
+}));
