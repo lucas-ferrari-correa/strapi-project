7fa944d76a5b2f1b4da4409371237281fb46a475
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2023-08-30 15:29:56 -0300

    extending strapi core 5 finished

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,44 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    strapi.db.lifecycles.subscribe({
+      models: ['admin::user'],
+      afterCreate: async ({ result }) => {
+        const { id, firstName, lastName, email, username, createdAt, updatedAt } = result;
+        await strapi.service('api::author.author').create({
+          data: {
+            firstName,
+            lastName,
+            email,
+            username,
+            createdAt,
+            updatedAt,
+            admin_user: [id]
+          }
+        })
+      },
+      afterUpdate: async ({ result }) => {
+        const correspondingAuthor = (await strapi.entityService.findMany("api::author.author", {
+          populate: ['admin_user'],
+          filters: {
+            admin_user: {
+              id: result.id
+            }
+          },
+        }))[0];
+
+        const { firstName, lastName, email, username, updatedAt } = result;
+        await strapi.service('api::author.author').update(correspondingAuthor.id, {
+          data: {
+            firstName,
+            lastName,
+            email,
+            username,
+            updatedAt
+          }
+        })
+      }
+    })
+  },
 };
